d3fbdaa55bc0199a2b437b1d397ef3f67a48d3fd
Сабилов Фархад <sabilov_farhad@mail.ru>
2021-09-19 02:56:50 +0300

    added components, toolbar, drawer

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import Dashboard from '@/components/Dashboard'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'reg',
+      component: Register
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
     }
-  ]
+  ],
+  mode: 'history'
 })
